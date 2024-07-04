2f1008c2c0abc7729c5d2b7dc125cd4078dd9c38
Luizmachado87 <luizmachado87@outlook.com>
2024-07-03 22:01:47 -0300

    project finally

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,32 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public ContaTerminal(){    
+    }
+    public static void main(String[] args) {
+        Scanner readingAgency = new Scanner(System.in);
+
+        System.out.println("Enter your agency number: ");
+        int numberAgency = readingAgency.nextInt();
+        System.out.println("The digital agency was: " + numberAgency);
+
+        Scanner readingAccount = new Scanner(System.in);
+
+        System.out.println("Enter your account number with the digit: ");
+        String numberAccount = readingAccount.nextLine();
+        System.out.println("The account number entered was: " + numberAccount);
+
+        Scanner readingName = new Scanner(System.in);
+
+        System.out.println("Enter your full name: ");
+        String name = readingName.nextLine();
+        System.out.println("The name entered was: " + name);
+
+        Scanner readingBalance = new Scanner(System.in);
+
+        System.out.println("Enter your balance: ");
+        String balance = readingBalance.nextLine();
+        System.out.println("Your available balance is: " + balance);
+
     }
 }
